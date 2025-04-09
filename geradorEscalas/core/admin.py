224797eee92ec03a70c49ec2e8b62fb78ea2fc9d
2104775UAb <159642349+2104775UAb@users.noreply.github.com>
2025-04-09 21:51:07 +0100

    Update admin.py

--- geradorEscalas/core/admin.py
+++ geradorEscalas/core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Permite alterar os seguintes modelos na admin view
-from .models import Militar, Dispensa, Escala, Servico, Configuracao
+from .models import Militar, Dispensa, Escala, Servico, Configuracao, Log
 
 admin.site.register(Militar)
 admin.site.register(Dispensa)
 admin.site.register(Escala)
 admin.site.register(Servico)
 admin.site.register(Configuracao)
+admin.site.register(Log)
