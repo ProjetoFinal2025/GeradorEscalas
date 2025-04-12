4dc8139b14970c3a40cafd7ded5b8eaa0a0cdb9d
2104775UAb <159642349+2104775UAb@users.noreply.github.com>
2025-04-12 19:13:54 +0100

    Atualização do design da página de login

--- Projeto/core/urls.py
+++ Projeto/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import login_view, home_view
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='login/', permanent=False)),
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
 ]
